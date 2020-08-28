97c55ef0bcd25b62ded9edd5b30e9fa45afcdf38
AswathyR1 <Aswathy.Rajeev@trv.envestnet.com>
2020-08-28 15:29:39 +0530

    Updates

---
+++ precache-manifest.c88fac31200f5df004a8de5c0e7bae4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "035af9aca6a983b8755c",
+    "url": "/portfolio/static/css/main.f7f23e01.chunk.css"
+  },
+  {
+    "revision": "035af9aca6a983b8755c",
+    "url": "/portfolio/static/js/main.035af9ac.chunk.js"
+  },
+  {
+    "revision": "a66675f864b1361fbcc8",
+    "url": "/portfolio/static/js/1.a66675f8.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "b7a46e4d3beb655d6b4bba9832dca0b3",
+    "url": "/portfolio/static/media/portfolio.b7a46e4d.jpg"
+  },
+  {
+    "revision": "2bbded225664d95eb65e21e43a32247e",
+    "url": "/portfolio/index.html"
+  }
+];
